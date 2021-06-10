f945096d45270e87ec62f478297cd3ac2b3fb192
Flávio Henrique Fernandes Volpon <flavio.fernandes@RIOSOFT.LOCAL>
2021-06-09 21:25:57 -0300

    Criado tela de listagem de eventos

--- core/views.py
+++ core/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from core.models import Evento
+from core import models
 
 # Create your views here.
+# def index(request):
+#     return redirect('/agenda')
+
+def lista_eventos(request):
+    # usuario = request.user
+    # eventos = Evento.objects.filter(usuario=usuario)
+    eventos = Evento.objects.all()
+    dados = {'eventos':eventos}
+    return render(request, 'agenda.html', dados)
