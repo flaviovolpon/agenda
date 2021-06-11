a41458ac3373d5b5aa0852aabd419b4c2cbcb878
Flávio Henrique Fernandes Volpon <flavio.fernandes@RIOSOFT.LOCAL>
2021-06-11 16:23:55 -0300

    adicionando funções de altAdicionado Http404, retorna lista de eventos por usuario em json e configurado timezone para pt-br

--- agenda/settings.py
+++ agenda/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-6+0$0ix=-@ea1=4_x##saq5#om$2#3s_y1=c)((hv@z0!xf=f(
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
@@ -104,15 +104,15 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/3.2/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = 'America/Sao_Paulo'
 
 USE_I18N = True
 
 USE_L10N = True
 
-USE_TZ = True
+USE_TZ = False
 
 
 # Static files (CSS, JavaScript, Images)
